a961e8f2e9c4795fbdee7e28fbd6b0eddd2c6aea
Franco Navarro <francotel@Francos-iMac.local>
2025-06-07 19:28:00 -0500

    update comments v1

--- 02-frontdoor.tf
+++ 02-frontdoor.tf
@@ -1,59 +1,58 @@
-resource "azurerm_cdn_frontdoor_profile" "fd-profile" {
-  name                     = "frontdoor-profile-${var.project}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  response_timeout_seconds = 16
-  sku_name                 = "Standard_AzureFrontDoor"
-  tags                     = var.tags
-}
+# resource "azurerm_cdn_frontdoor_profile" "fd-profile" {
+#   name                     = "frontdoor-profile-${var.project}"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   response_timeout_seconds = 16
+#   sku_name                 = "Standard_AzureFrontDoor"
+#   tags                     = var.tags
+# }
 
-resource "azurerm_cdn_frontdoor_endpoint" "web" {
-  name                     = "web"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd-profile.id
-  tags                     = var.tags
-}
+# resource "azurerm_cdn_frontdoor_endpoint" "web" {
+#   name                     = "web"
+#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd-profile.id
+#   tags                     = var.tags
+# }
 
-resource "azurerm_cdn_frontdoor_rule_set" "cache" {
-  name                     = "caching"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd-profile.id
-}
+# resource "azurerm_cdn_frontdoor_rule_set" "cache" {
+#   name                     = "caching"
+#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd-profile.id
+# }
 
-resource "azurerm_cdn_frontdoor_origin_group" "web" {
-  name                     = "web"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd-profile.id
+# resource "azurerm_cdn_frontdoor_origin_group" "web" {
+#   name                     = "web"
+#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd-profile.id
 
-  load_balancing {
-    additional_latency_in_milliseconds = 0
-    sample_size                        = 16
-    successful_samples_required        = 3
-  }
-}
+#   load_balancing {
+#     additional_latency_in_milliseconds = 0
+#     sample_size                        = 16
+#     successful_samples_required        = 3
+#   }
+# }
 
-resource "azurerm_cdn_frontdoor_rule" "cache" {
-  name                      = "static"
-  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.cache.id
-  order                     = 1
-  behavior_on_match         = "Stop"
+# resource "azurerm_cdn_frontdoor_rule" "cache" {
+#   name                      = "static"
+#   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.cache.id
+#   order                     = 1
+#   behavior_on_match         = "Stop"
 
-  conditions {
-    url_file_extension_condition {
-      operator     = "Equal"
-      match_values = ["css", "js", "ico", "png", "jpeg", "jpg", ".map"]
-    }
-  }
+#   conditions {
+#     url_file_extension_condition {
+#       operator     = "Equal"
+#       match_values = ["css", "js", "ico", "png", "jpeg", "jpg", ".map"]
+#     }
+#   }
 
-  actions {
-    route_configuration_override_action {
-      compression_enabled = true
-      cache_behavior      = "HonorOrigin"
-    }
-  }
+#   actions {
+#     route_configuration_override_action {
+#       compression_enabled = true
+#       cache_behavior      = "HonorOrigin"
+#     }
+#   }
 
-  depends_on = [
-    azurerm_cdn_frontdoor_origin_group.web,
-    azurerm_cdn_frontdoor_origin.web
-  ]
-}
-
+#   depends_on = [
+#     azurerm_cdn_frontdoor_origin_group.web,
+#     azurerm_cdn_frontdoor_origin.web
+#   ]
+# }
 
 
 # resource "azurerm_cdn_frontdoor_origin" "web" {
